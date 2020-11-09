0d6ae43ec46922fae859321b62664efac4900067
pisce <pis.ceyatta@gmail.com>
2020-11-10 00:35:28 +0900

    sfen ロードできるぞ！！！

--- src/info.py
+++ src/info.py
@@ -2,3 +2,14 @@ ratio = 31.7 / 34.8     # width / height
 
 hand_ratio = .9
 hand_space = 10
+
+all_piece = {
+    'k',
+    'r',
+    'b',
+    'g',
+    's',
+    'n',
+    'l',
+    'p'
+}
---
+++ src/load_sfen.py
@@ -0,0 +1,80 @@
+import info
+
+test = 'sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1'
+
+class Board:
+    def __init__(self):
+        self.board = [['' for _ in range(9)] for _ in range(9)]
+        self.isComp = False
+        self.now = [0, 0]
+    
+    def add_info(self, command, *param):
+        if self.isComp:
+            raise Exception("盤面は既に完成しています")
+
+        if command == 'skip':
+            # 盤面スキップ回数
+            self.check_now()
+            count, = param
+            self.now[1] += count
+        elif command == 'add':
+            # 情報を入力
+            self.check_now()
+            x, y = self.now
+            self.board[x][y] = '+' * param[1] + param[0]
+            self.now[1] += 1
+        elif command == 'next':
+            # 次の行に
+            self.now[0] += 1
+            self.now[1] = 0
+        elif command == 'finish':
+            # 終了する
+            self.isComp = True
+        else:
+            raise Exception("不正なcommandです:", command)
+    
+    def check_now(self):
+        if self.now[0] >= 9 or self.now[1] >= 9:
+            raise Exception("範囲外の指定があります")
+
+
+
+class ParseSfen:
+    def __init__(self, sfen):
+        self.sfen = sfen
+        self.info = Board()
+        self.parse()
+    
+    def parse(self):
+        sfen_data = self.sfen.split(" ")
+        if sfen_data[0] == 'sfen':
+            board, *board_data = sfen_data[1:]
+        
+        # 盤面の読み込み
+        print(board, board_data)
+        isGradeUp = False
+        for piece in board:
+            if piece.isdecimal():
+                if isGradeUp:
+                    raise Exception("+の後に数字がくることはありません。")
+                self.info.add_info('skip', int(piece))
+            elif piece == '+':
+                if isGradeUp:
+                    raise Exception("'++' の表記があります。")
+                isGradeUp = True
+            elif piece.lower() in info.all_piece:
+                self.info.add_info('add', piece, isGradeUp)
+                isGradeUp = False
+            elif piece == '/':
+                if isGradeUp:
+                    raise "'+/' という表記が存在します。"
+                self.info.add_info('next')
+            else:
+                raise Exception("不正なパラメータです:", piece)
+            
+        self.info.add_info('finish')
+
+
+if __name__ == "__main__":
+    board = ParseSfen(test)
+    print(*board.info.board, sep='\n')
