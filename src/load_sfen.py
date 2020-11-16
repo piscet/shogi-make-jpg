d7b54b83fa9e9bff203032b3bfa86dc8025e1ba0
pisce <pis.ceyatta@gmail.com>
2020-11-16 14:09:34 +0900

    手番とか手数とかを読み込ませたぜやったー！

--- src/load_sfen.py
+++ src/load_sfen.py
@@ -5,9 +5,12 @@ test = 'sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1'
 class Board:
     def __init__(self):
         self.board = [['' for _ in range(9)] for _ in range(9)]
+        self.turn = None
+        self.num_of_turn = None
         self.isComp = False
         self.now = [0, 0]
-    
+        self.isComp_hand = False
+
     def add_info(self, command, *param):
         if self.isComp:
             raise Exception("盤面は既に完成しています")
@@ -32,11 +35,45 @@ class Board:
             self.isComp = True
         else:
             raise Exception("不正なcommandです:", command)
-    
+
+    def add_hand(self, command, *param):
+        if command == 'add':
+            # 情報を入力
+            pass
+        elif command == 'finish':
+            # 終了する
+            pass
+        else:
+            raise Exception("不正なcommandです:", command)
+
+    def set_turn(self, turn):
+        if turn in {'b', 'w'}:
+            self.turn = (turn == 'b')
+        else:
+            raise Exception("不正な値です:", turn)
+
+    def set_num_of_turn(self, num):
+        self.num_of_turn = int(num)
+
     def check_now(self):
         if self.now[0] >= 9 or self.now[1] >= 9:
             raise Exception("範囲外の指定があります")
 
+
+class Piece:
+    def __init__(self, piece, isGradeUp = False):
+        if piece == "":
+            # 空の盤面を表す
+            self.piece = ""
+            self.turn = None
+            self.isGradeUp = None
+        else:
+            _piece = piece.lower()
+            if not _piece in info.all_piece:
+                raise Exception("不正なパラメータです:", piece)
+            self.piece = _piece             # 常に小文字
+            self.turn = (piece == _piece)   # true : 先手
+            self.isGradeUp = isGradeUp      # 成っているかどうか
 
 
 class ParseSfen:
@@ -44,12 +81,12 @@ class ParseSfen:
         self.sfen = sfen
         self.info = Board()
         self.parse()
-    
+
     def parse(self):
         sfen_data = self.sfen.split(" ")
         if sfen_data[0] == 'sfen':
             board, *board_data = sfen_data[1:]
-        
+
         # 盤面の読み込み
         print(board, board_data)
         isGradeUp = False
@@ -71,10 +108,26 @@ class ParseSfen:
                 self.info.add_info('next')
             else:
                 raise Exception("不正なパラメータです:", piece)
-            
+
         self.info.add_info('finish')
+
+        # 手番
+        self.info.set_turn(board_data[0])
+
+        # 手駒
+        if sfen_data[1] == '-':
+            self.info.add_hand('finish')
+        else:
+            for piece in sfen_data[1]:
+                self.info.add_hand('add', piece)
+            self.info.add_hand('finish')
+
+        # 手数
+        self.info.set_num_of_turn(board_data[2])
 
 
 if __name__ == "__main__":
     board = ParseSfen(test)
     print(*board.info.board, sep='\n')
+    print("手番 :", "Black" if board.info.turn else "White")
+    print("手数 :", board.info.num_of_turn)
