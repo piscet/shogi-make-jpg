5e1fa90593a388ce97e5cfbeb54efd31f4e25f47
pisce <pis.ceyatta@gmail.com>
2020-11-17 01:55:33 +0900

    add alphabet to 駒文字

--- src/info.py
+++ src/info.py
@@ -13,3 +13,27 @@ all_piece = {
     'l',
     'p'
 }
+
+alphToJa = {
+    'k' : '玉',
+    'r' : '飛',
+    'b' : '角',
+    'g' : '金',
+    's' : '銀',
+    'n' : '桂',
+    'l' : '香',
+    'p' : '歩',
+}
+
+alphaToJa_nari = {
+    'r' : '竜',
+    'b' : '馬',
+    's' : '全',
+    'n' : '圭',
+    'l' : '杏',
+    'p' : 'と',
+}
+
+font_size_board = 42
+# font_size_hand = 15
+font_pth = 'C:\\Windows\\Fonts\\Arial\\BIZ-UDGothicR.ttc'
